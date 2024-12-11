f1f48ce586cde3a5dabcd9a9660b504b9630cb52
Maciej Bratko <157412862+MaciejBratko@users.noreply.github.com>
2024-12-11 20:52:29 +0100

    hw06-email (#7)
    * done
    
    * Docker
    
    * small fix

--- models/user.js
+++ models/user.js
@@ -24,6 +24,14 @@ const userSchema = new Schema({
     type: String,
     default: null,
   },
+  verify: {
+    type: Boolean,
+    default: false,
+  },
+  verificationToken: {
+    type: String,
+    default: null,
+  },
 });
 
 const User = mongoose.model("user", userSchema);
